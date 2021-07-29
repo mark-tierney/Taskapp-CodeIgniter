57efdac737c3c1caa3791e6edcc409a71d28d341
mark-tierney <marktierney66@gmail.com>
2021-07-29 15:16:20 -0700

    db connected, basic task MVC implemented

---
+++ app/Controllers/Tasks.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Controllers;
+
+class Tasks extends BaseController
+{
+	public function index()
+	{
+		$model = new \App\Models\TaskModel;
+		$data = $model->findAll();
+
+		//dd($data);
+
+		return view("Tasks/index", ['tasks' => $data]);
+	}
+}
---
+++ app/Views/Home/index.php
@@ -0,0 +1,11 @@
+<?= $this->extend("layouts/default") ?>
+
+<?= $this->section("title") ?>
+Home
+<?= $this->endSection() ?>
+
+<?= $this->section("content") ?>
+
+home
+
+<?= $this->endSection() ?>
